0d875a66810e6d8f868aad52b020e604e10e5e6c
Hikmat Kakar <hkakar2@horizon.csueastbay.edu>
2025-07-30 21:19:20 -0700

    fix

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,11 @@ const Card = ({name, email, id}) => {
 	
 	return (
 		<div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'> 
-			<img alt='robots' src={`https://robohash.org/${id}?200x200`} /> 
+			<img alt='robots' src={`https://robohash.org/${id}?200x200`} 
+				width="268"
+ 				 height="268"
+  				style={{ objectFit: 'cover' }}
+			/> 
 		<div>
 			<h2>{name}</h2> 
 			<p>{email}</p>
