8737a486ed7d778b17c4c367337df63448f683a6
Hikmat Kakar <hkakar2@horizon.csueastbay.edu>
2025-07-31 00:27:01 -0700

    push

--- src/service-worker.js
+++ src/service-worker.js
@@ -5,6 +5,8 @@ import { ExpirationPlugin } from 'workbox-expiration';
 import { precacheAndRoute, createHandlerBoundToURL } from 'workbox-precaching';
 import { registerRoute } from 'workbox-routing';
 import { StaleWhileRevalidate } from 'workbox-strategies';
+import { CacheFirst } from 'workbox-strategies';
+
 
 clientsClaim();
 
